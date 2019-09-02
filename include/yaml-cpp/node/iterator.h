5de1d120e3920893db29d010cb98d2aea4247c87
André Malcher <malchera@users.noreply.github.com>
2019-09-02 10:58:07 +0200

    Structured bindings only supported from C++17

--- include/yaml-cpp/node/iterator.h
+++ include/yaml-cpp/node/iterator.h
@@ -30,6 +30,7 @@ struct iterator_value : public Node, std::pair<Node, Node> {
 }
 }
 
+#if __cplusplus >= 201703L
 namespace std {
   template <> struct tuple_size<YAML::detail::iterator_value>
       : std::integral_constant<std::size_t, 2> {};
@@ -41,5 +42,6 @@ namespace std {
     using type = YAML::detail::iterator_value::second_type;
   };
 }
+#endif // __cplusplus >= 201703L
 
 #endif  // VALUE_ITERATOR_H_62B23520_7C8E_11DE_8A39_0800200C9A66
